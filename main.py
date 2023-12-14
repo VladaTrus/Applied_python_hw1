ae7ef4792f3e98ff3e99f5d2eaaef53d0de12e85
VladaTrus <47668051+VladaTrus@users.noreply.github.com>
2023-12-14 15:53:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title('EDA')
+
+cols = ['AGREEMENT_RK',
+'TARGET',
+'AGE',
+'SOCSTATUS_WORK_FL',
+'SOCSTATUS_PENS_FL',
+'GENDER',
+'CHILD_TOTAL',
+'DEPENDANTS',
+'PERSONAL_INCOME',
+'LOAN_NUM_TOTAL',
+'LOAN_NUM_CLOSED']
+
+option = st.selectbox(
+    'Распределение признака:',
+    cols)
+
+st.write(option)
